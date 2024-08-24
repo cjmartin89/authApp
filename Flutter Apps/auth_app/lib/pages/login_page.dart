8fe081b3293961f31807df008c70a0e197903bdd
Chris Martin <cjmartin89@proton.me>
2024-08-23 22:59:10 -0400

    UI Finished

--- Flutter Apps/auth_app/lib/pages/login_page.dart
+++ Flutter Apps/auth_app/lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:auth_app/components/my_button.dart';
 import 'package:auth_app/components/my_textfield.dart';
+import 'package:auth_app/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -19,6 +20,7 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 50),
 
@@ -83,29 +85,70 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 25),
 
               // or continue with
-              Row(
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              // google + apple sign in button
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey[400],
-                    ),
-                  ),
-                  const Text(
-                    'Or continue with',
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey[400],
-                    ),
-                  ),
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  const SizedBox(width: 10),
+                  
+                  // apple button
+                  SquareTile(imagePath: 'lib/images/apple.png'),
                 ],
               ),
 
-              // google + apple sign in button
+              const SizedBox(height: 10),
 
               //not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?',
+                  style: TextStyle(color: Colors.grey[700]),
+                  ),
+
+                  const SizedBox(width: 4),
+
+                  const Text(
+                    'Register Now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
