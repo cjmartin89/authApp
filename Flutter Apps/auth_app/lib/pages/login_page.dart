955b63559d90a9ec86e838294598910bbec5f61c
Chris Martin <cjmartin89@proton.me>
2024-08-23 23:27:56 -0400

    Install Firebase

--- Flutter Apps/auth_app/lib/pages/login_page.dart
+++ Flutter Apps/auth_app/lib/pages/login_page.dart
@@ -121,7 +121,7 @@ class LoginPage extends StatelessWidget {
                   // google button
                   SquareTile(imagePath: 'lib/images/google.png'),
 
-                  const SizedBox(width: 10),
+                  SizedBox(width: 10),
                   
                   // apple button
                   SquareTile(imagePath: 'lib/images/apple.png'),
