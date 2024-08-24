fb78c00a8407a648f4c9671b98e12bb081593913
Chris Martin <cjmartin89@proton.me>
2024-08-23 22:26:28 -0400

    On branch main Initial commit

---
+++ Flutter Apps/auth_app/lib/pages/login_page.dart
@@ -0,0 +1,90 @@
+import 'package:auth_app/components/my_button.dart';
+import 'package:auth_app/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(height: 50),
+
+              // welcome back, you've been missed!
+              (Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              )),
+
+              const SizedBox(height: 25),
+
+              //username textField
+              MyTextfield(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+                ),
+
+              const SizedBox(height: 25),
+
+              //password textField
+              MyTextfield(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              //forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password',
+                      style: TextStyle(color: Colors.grey[600]),
+                    )
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              //sign in bottom
+              MyButton(
+                onTap: () {} ),
+
+              // or continue with
+
+              // google + apple sign in button
+
+              //not a member? register now
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
